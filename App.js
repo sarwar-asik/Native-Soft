f02c4902580fcd3159f757b1dbbbd79b2a600bee
sarwar-asik <sarwarasik@gmail.com>
2024-06-29 12:34:24 +0600

    features(footer):created footer buttons

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sarwar ISO App!</Text>
-      <Text>Full Stack Software Engineer</Text>
-      <StatusBar style="auto" />
-      <Image
-        source={{
-          uri: "https://avatars.githubusercontent.com/u/108589375?v=4",
-        }}
-        style={{ width: 100, height: 100 }}
-      />
       {/* View */}
       <View style={styles.second}>
+        <Text>Sarwar ISO App!</Text>
+        <Text style={{ marginTop: 10, marginBottom: 20 }}>
+          Full Stack Software Engineer
+        </Text>
+        <StatusBar style="auto" />
+        <Image
+          source={{
+            uri: "https://avatars.githubusercontent.com/u/108589375?v=4",
+          }}
+          style={{ width: 100, height: 100 }}
+        />
         {/* Text */}
         <Text style={{ color: "white" }}>First Android</Text>
       </View>
@@ -25,6 +27,26 @@ export default function App() {
           style={{ width: 50, height: 50 }}
         />
       </View>
+      <View style={styles.footer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Home")}
+        >
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Note")}
+        >
+          <Text style={styles.buttonText}>Note</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Profile")}
+        >
+          <Text style={styles.buttonText}>Profile</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -34,18 +56,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
-    gap: 20,
+    justifyContent: "space-between",
+    gap: 30,
   },
   second: {
-    width: 100,
+    width: 200,
     height: 100,
     backgroundColor: "red",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    gap: 3,
-    marginTop: 10,
+    gap: 20,
+    marginTop: 120,
     borderRadius: 5,
     borderWidth: 1,
     borderColor: "black",
@@ -56,5 +78,22 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    height: 60,
+    backgroundColor: "#f8f8f8",
+    borderTopWidth: 1,
+    borderColor: "#e7e7e7",
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#007aff",
+    fontSize: 16,
   },
 });
