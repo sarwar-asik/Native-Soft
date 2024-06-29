83a88bb2854687a52e991c93cf2d188a9dc31276
sarwar-asik <sarwarasik@gmail.com>
2024-06-29 12:25:18 +0600

    fetures(core-components):created View, Image and styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -7,6 +7,24 @@ export default function App() {
       <Text>Sarwar ISO App!</Text>
       <Text>Full Stack Software Engineer</Text>
       <StatusBar style="auto" />
+      <Image
+        source={{
+          uri: "https://avatars.githubusercontent.com/u/108589375?v=4",
+        }}
+        style={{ width: 100, height: 100 }}
+      />
+      {/* View */}
+      <View style={styles.second}>
+        {/* Text */}
+        <Text style={{ color: "white" }}>First Android</Text>
+      </View>
+      <View>
+        {/* Image */}
+        <Image
+          source={require("./assets/favicon.png")}
+          style={{ width: 50, height: 50 }}
+        />
+      </View>
     </View>
   );
 }
@@ -17,5 +35,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    gap: 20,
+  },
+  second: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 3,
+    marginTop: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "black",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
 });
